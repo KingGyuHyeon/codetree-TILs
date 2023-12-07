c999e83414d3443b6ff2d8b9a7d2676f4855013b
KingGyuHyeon <147672312+KingGyuHyeon@users.noreply.github.com>
2023-12-07 13:33:34 +0900

    Title: 2가 3번째로 등장하는 위치, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231207/2가 3번째로 등장하는 위치/where-2-appears-3rd.c
+++ 231207/2가 3번째로 등장하는 위치/where-2-appears-3rd.c
@@ -4,16 +4,19 @@
 int main() {
     int n;
     int *p;
-    int i, cnt = 0;
+    int i, count = 0, t = 2;
     scanf("%d", &n);
-    p = (int *)malloc(sizeof(int)*n);
+    p = (int *)malloc(sizeof(int) * n);
     for(i = 0; i < n; i++) {
         scanf("%d", &p[i]);
-        cnt++;
-        if(cnt==3) {
-        printf("%d", i+1);
-        break;
+        if (p[i] == t) {
+            count++;
+            if (count == 3) {
+                printf("%d\n", i + 1);
+                break;
+            }
         }
     }
+    free(p);
     return 0;
 }
